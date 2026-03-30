0c1fc41cb16f02e63750f40c421b0c0f94515c38
Vietor Liu <vietor.liu@gmail.com>
2026-03-30 20:45:32 +0800

    Refactory command line parse

--- bin/serun.js
+++ bin/serun.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { loadAllEnvs, executeCommand } = require('../lib');
+const { loadAllEnvs, executeCommand } = require("../lib");
 
 function showHelp() {
   console.log(`Usage: serun [options] <command> [args...]
@@ -15,33 +15,58 @@ Optionally load a channel-specific file after the default file.
   process.exit(0);
 }
 
-function main() {
-  const args = process.argv.slice(2);
+function parseCommandLine(args) {
+  const program = {
+    help: false,
+    channel: null,
+    command: null,
+    commandArgs: [],
+  };
 
-  if (args.length === 0 || args.includes('--help') || args.includes('-h')) {
-    showHelp();
+  if (args.length === 0) {
+    program.help = true;
   }
 
-  let channel = null;
-
   for (let i = 0; i < args.length; i++) {
-    if (args[i] === '--channel' && i + 1 < args.length) {
-      channel = args[i + 1];
-      args.splice(i, 2);
+    const name = args[i];
+    if (!name.startsWith("-")) {
       break;
-    } else if (args[i] === '-c' && i + 1 < args.length) {
-      channel = args[i + 1];
-      args.splice(i, 2);
-      break;
+    }
+
+    if (name === "--help" || name === "-h") {
+      program.help = true;
+      args.splice(i, 1);
+    } else if (name === "--channel" || name === "-c") {
+      if (i + 1 < args.length) {
+        program.channel = args[i + 1];
+        args.splice(i, 2);
+      } else {
+        showHelp();
+      }
+    } else {
+      showHelp();
     }
   }
 
-  const envs = loadAllEnvs(channel);
+  if (args.length > 0) {
+    program.command = args[0];
+  }
 
-  const command = args[0];
-  const commandArgs = args.slice(1);
+  if (args.length > 1) {
+    program.commandArgs = args.slice(1);
+  }
 
-  executeCommand(command, commandArgs, envs);
+  return program;
+}
+
+function main() {
+  const program = parseCommandLine(process.argv.slice(2));
+  if (program.help) {
+    showHelp();
+  }
+
+  const envs = loadAllEnvs(program.channel);
+  executeCommand(program.command, program.commandArgs, envs);
 }
 
 main();
